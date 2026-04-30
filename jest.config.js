164aee3d1f9f22144a0a41e39c0ce27b48d68210
Justin Aguila <107596188+jraa1995@users.noreply.github.com>
2026-04-30 08:10:17 -0400

    Exclude code.js from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/tests/setup.js'],
-  testMatch: ['<rootDir>/tests/**/*.test.js'],
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/tests/setup.js"],
+  testMatch: ["<rootDir>/tests/**/*.test.js"],
   collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/Triggers.js'
+    "src/**/*.js",
+    "!src/Triggers.js",
+    "!src/Code.js",
   ],
   coverageThreshold: {
     global: {
       branches: 70,
       functions: 70,
       lines: 70,
-      statements: 70
-    }
+      statements: 70,
+    },
   },
-  coverageReporters: ['text', 'lcov', 'html']
-}
+  coverageReporters: ["text", "lcov", "html"],
+};
